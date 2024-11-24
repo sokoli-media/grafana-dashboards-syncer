42c0b566006e7cfa96e6422cf9518226710ec9c3
Maciej Plonski <info@sokoli.pl>
2024-11-24 17:38:39 +0100

    add basic prometheus metrics

--- internal/syncer/dashboards_directory.go
+++ internal/syncer/dashboards_directory.go
@@ -1,9 +1,20 @@
 package syncer
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"os"
 	"path/filepath"
 )
+
+var directoryListFromDrive = promauto.NewCounter(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_directory_list_from_drive"})
+var directoryListFromCache = promauto.NewCounter(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_directory_list_from_cache"})
+var directorySaveDashboard = promauto.NewCounter(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_directory_save"})
+var directoryRemoveDashboard = promauto.NewCounter(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_directory_remove"})
 
 func NewDashboardsDirectory(dashboardsDirectory string) *DashboardsDirectory {
 	var existingFiles []string
@@ -20,6 +31,7 @@ type DashboardsDirectory struct {
 
 func (d DashboardsDirectory) saveDashboard(dashboard Dashboard) error {
 	*d.existingFiles = nil
+	directorySaveDashboard.Inc()
 
 	fullPath := filepath.Join(d.directoryPath, dashboard.filename)
 	return os.WriteFile(fullPath, []byte(dashboard.dashboard), 0644)
@@ -27,6 +39,8 @@ func (d DashboardsDirectory) saveDashboard(dashboard Dashboard) error {
 
 func (d DashboardsDirectory) listDashboards() ([]string, error) {
 	if len(*d.existingFiles) == 0 {
+		directoryListFromDrive.Inc()
+
 		entries, err := os.ReadDir(d.directoryPath)
 		if err != nil {
 			return nil, err
@@ -42,6 +56,8 @@ func (d DashboardsDirectory) listDashboards() ([]string, error) {
 		}
 
 		*d.existingFiles = dashboards
+	} else {
+		directoryListFromCache.Inc()
 	}
 
 	return *d.existingFiles, nil
@@ -49,6 +65,7 @@ func (d DashboardsDirectory) listDashboards() ([]string, error) {
 
 func (d DashboardsDirectory) removeDashboard(filename string) error {
 	*d.existingFiles = nil
+	directoryRemoveDashboard.Inc()
 
 	fullPath := filepath.Join(d.directoryPath, filename)
 	return os.Remove(fullPath)
--- internal/syncer/dashboards_syncer.go
+++ internal/syncer/dashboards_syncer.go
@@ -1,8 +1,20 @@
 package syncer
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"log/slog"
 )
+
+var dashboardUpdated = promauto.NewCounterVec(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_updated"},
+	[]string{"dashboard"})
+var dashboardNotUpdated = promauto.NewCounterVec(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_dashboard_not_updated"},
+	[]string{"dashboard"})
+var oldDashboardCleanedUp = promauto.NewCounterVec(
+	prometheus.CounterOpts{Name: "grafana_dashboards_syncer_old_dashboard_cleaned_up"},
+	[]string{"dashboard"})
 
 type DashboardsSyncer struct {
 	logger            *slog.Logger
@@ -17,6 +29,7 @@ func (d DashboardsSyncer) downloadDashboards() DownloadedDashboards {
 
 	for _, dashboard := range d.downloader.downloadAllDashboards() {
 		if d.currentDashboards.dashboardHasBeenUpdated(dashboard) {
+			dashboardUpdated.WithLabelValues(dashboard.filename).Inc()
 			err := d.directory.saveDashboard(dashboard)
 			if err != nil {
 				d.logger.Error("saving dashboard failed", "error", err)
@@ -26,6 +39,7 @@ func (d DashboardsSyncer) downloadDashboards() DownloadedDashboards {
 			d.currentDashboards.saveDashboard(dashboard)
 			d.logger.Info("saved new or updated dashboard", "filename", dashboard.filename)
 		} else {
+			dashboardNotUpdated.WithLabelValues(dashboard.filename).Inc()
 			d.logger.Info("dashboard is the same, ignoring", "filename", dashboard.filename)
 		}
 
@@ -44,6 +58,8 @@ func (d DashboardsSyncer) cleanUpDashboards(downloadedDashboards DownloadedDashb
 
 	for _, filename := range existingDashboards {
 		if !downloadedDashboards.hasBeenDownloaded(filename) {
+			oldDashboardCleanedUp.WithLabelValues(filename).Inc()
+
 			d.logger.Info("removing old dashboard", "filename", filename)
 			err = d.directory.removeDashboard(filename)
 			if err != nil {
